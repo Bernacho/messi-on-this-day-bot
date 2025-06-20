2510feccae78dbca3a6cfd399c1f5fb7a9cf040b
Bernardo Lopez <Bernardo.LopezVicencio@asr-group.com>
2025-06-20 13:39:34 -0600

    update

--- messi_bot.py
+++ messi_bot.py
@@ -18,18 +18,24 @@ def check_and_tweet():
     print(f"Checking schedule at {now}")
 
     df = pd.read_csv("data/messi_goals_with_goal_datetime.csv")
-    df['goal_datetime'] = pd.to_datetime(df.goal_datetime).apply(lambda x: x.replace(year=today.year))
-    df['time'] = df.goal_datetime.dt.time
-    df['goal_datetime'] = df['goal_datetime'].dt.floor("min")
 
-    matching_goals = df[df.goal_datetime.between(now,end,inclusive='both')].sort_values(["goal_datetime","time"]).reset_index(drop=True)
+    df['goal_datetime'] = pd.to_datetime(df.goal_datetime).apply(lambda x: x.replace(year=today.year))
+    df['goal_datetime_floor'] = df.goal_datetime.copy().dt.floor("min")
+
+    matching_goals = df[df.goal_datetime_floor.between(now,end,inclusive='both')].sort_values(["goal_datetime"]).reset_index(drop=True)
 
     for i, g_ in matching_goals.iterrows():
+        if i==0:
+            wait_sec = (g_.goal_datetime - now).total_seconds() - SETUP_TIME_SEC
+        else:
+            wait_sec = g_.wait_sec - PROCESSING_TIME_SEC
+
+        time.sleep(max(0,wait_sec))
+
         print(f"Tweeting goal: {g_.id}")
         text_, gif_file_ = create_tweet(g_)
         publish_tweet(text_, gif_file_)
-        if g_.wait_min<=WAITING_MINUTES:
-            time.sleep((g_.wait_min*60) - SETUP_TIME_SEC - ((i+1)*PROCESSING_TIME_SEC))
+
 
 if __name__ == "__main__":
     check_and_tweet()
