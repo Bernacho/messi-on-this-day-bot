36ea5bf7fa4f558299b32484a20446dd052ed766
Bernardo Lopez <Bernardo.LopezVicencio@asr-group.com>
2025-06-26 12:31:35 -0600

    Retry tweets

--- messi_bot.py
+++ messi_bot.py
@@ -14,8 +14,11 @@ app = Flask(__name__)
 
 @app.route("/check_goals")
 def check_and_tweet():
+    MAX_RETRIES = 3
+    PUBLISHED_TWEETS = 0
+
     now = datetime.now(UTC).replace(second=0,microsecond=0)
-    end = now + timedelta(minutes=WAITING_MINUTES)
+    # end = now + timedelta(minutes=WAITING_MINUTES)
     today = now.date()
 
     print(f"Checking schedule at {now}")
@@ -25,7 +28,7 @@ def check_and_tweet():
     df['goal_datetime'] = pd.to_datetime(df.goal_datetime).apply(lambda x: x.replace(year=today.year))
     df['goal_datetime_floor'] = df.goal_datetime.copy().dt.floor("min")
 
-    matching_goals = df[df.goal_datetime_floor.between(now,end,inclusive='both')].sort_values(["goal_datetime"]).reset_index(drop=True)
+    matching_goals = df[df.goal_datetime_floor==now].sort_values(["goal_datetime"]).reset_index(drop=True)
     
     for i, g_ in matching_goals.iterrows():
         # No waiting
@@ -35,17 +38,25 @@ def check_and_tweet():
         #     wait_sec = g_.wait_sec - PROCESSING_TIME_SEC
         # time.sleep(max(0,wait_sec))
         
-        print(f"Tweeting goal: {g_.id}")
-        text_, gif_file_ = create_tweet(g_)
-        publish_tweet(text_, gif_file_)
+        for attempt in range(1, MAX_RETRIES + 1):
+            try:
+                print(f"Tweeting goal: {g_.id}")
+                text_, gif_file_ = create_tweet(g_)
+                publish_tweet(text_, gif_file_)
+                PUBLISHED_TWEETS+=1
+                break
+            except Exception as e:
+                print(f"Attempt {attempt} failed: {e}")
+                if attempt == MAX_RETRIES:
+                    print("All retry attempts failed.")
 
-    l_=matching_goals.shape[0]
-    if l_>0:
-        m_ = f"{l_} Tweets {now}"
+
+    if PUBLISHED_TWEETS>0:
+        m_ = f"{PUBLISHED_TWEETS} Tweets {now}"
     else:
         m_ = f"No goals found {now}"
-    
     print(m_)
+
     return m_
 
 
